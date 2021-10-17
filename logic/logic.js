a2b3449a7066fd2c45737084f1ceda9f229fc9d9
Sasha <aadavidenkoweb@yandex.ru>
2021-10-17 22:51:41 +0700

    Добавил страницу регистрации и валидацию

--- logic/logic.js
+++ logic/logic.js
@@ -1,7 +1,14 @@
 const addWorkspace = document.querySelector('.right-block-plus');
 const allBlocks = document.querySelector('.right-blocks');
 const addOneLevel = document.querySelector('.title-add');
-const oneLevel = document.querySelector('.one-level');
+const levelsBlock = document.querySelector('.levels-block');
+const allTabs = document.querySelector('.right-tabs');
+const addTabInput = document.querySelector('.right-add-tab');
+const btnAddTab = document.querySelector('.btn-add-tab');
+const titleText = document.querySelector('.title-text');   
+const levelsItems = document.querySelector('.levels-items');     
+
+
 
 
 addWorkspace.addEventListener('click', () => {
@@ -13,8 +20,28 @@ addWorkspace.addEventListener('click', () => {
 addOneLevel.addEventListener('click', () => {
     const newOneLevel = document.createElement('p');
     newOneLevel.classList.add('one-level');
-    oneLevel.append(newOneLevel);
+    newOneLevel.innerText = titleText.value;
+
+    levelsBlock.appendChild(newOneLevel);
 });
+
+btnAddTab.addEventListener('click', () => {
+    const newTab = document.createElement('div');
+    newTab.classList.add('right-tab');
+    const newText = addTabInput.value;
+    const emptyStr = newText.trim() === '';
+    if (emptyStr === true) {
+        console.log('Не корректный ввод');
+    } else {
+        newTab.innerText = newText;
+        allTabs.classList.add('right-tab-text');
+        allTabs.append(newTab);
+    }
+});
+
+
+
+
 
 
 
